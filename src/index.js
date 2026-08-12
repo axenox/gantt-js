5a310a11d62f19b71e499d5cf966096b3b620462
Sergej Riel <sergej.riel@outlook.de>
2026-08-12 12:31:46 +0200

    Temp publish test.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ export default class Gantt {
 
     setup_wrapper(element) {
         let svg_element, wrapper_element;
+        let publishTestDeleteIt = false; //TODO SR: Entferne
 
         // CSS Selector is passed
         if (typeof element === 'string') {
